d2f5109cf5986cf92933fda9066274fc4fd1d3a1
vanajachowdary16 <vnajachowdary12@gmail.com>
2025-05-15 16:00:17 +0530

    deleted folder

--- JavaExerciseV/src/java/Strings/ReverseString.java
+++ JavaExerciseV/src/java/Strings/ReverseString.java
@@ -1,5 +1,19 @@
 package java.Strings;
 
 public class ReverseString {
+	  public static void main(String[] args) {
+	        System.out.println("Working with string");
+	    
+	        
+	        String s = "Iam learning Strings Concept in java";
+	        String[] words = s.split(" ");
+	        StringBuilder reverse = new StringBuilder();
+	        for(int i=words.length-1; i>=0; i--)
+	        {
+	            reverse.append(words[i]).append(" ");
+	        }
+	         System.out.println(reverse.toString().trim());
+	   
+	}
 
 }
