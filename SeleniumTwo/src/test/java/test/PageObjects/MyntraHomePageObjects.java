429c01d06920130d24ebe5defc355879eba79e15
vanajachowdary16 <vnajachowdary12@gmail.com>
2025-05-21 20:01:11 +0530

    findinf links at specified section

--- SeleniumTwo/src/test/java/test/PageObjects/MyntraHomePageObjects.java
+++ SeleniumTwo/src/test/java/test/PageObjects/MyntraHomePageObjects.java
@@ -6,8 +6,8 @@ public class MyntraHomePageObjects extends WebUtilis{
 	public static String myntraHomeURL="https://www.myntra.com/";
 	public static String menCssSelector="a.desktop-main[href='/shop/men']";
 	public static String TshirsXpath="//a[@class='desktop-categoryLink' and text()='T-Shirts']";
-	
-	
+	public static String footerXpath="//footer[@class='desktop-footerContainer']";
+	public static String popularSearch="//div[@class='desktop-pSearchlinks']";
 
 
 }
--- SeleniumTwo/src/test/java/test/scripts/FindFooterSectionLinsOnMyntraHomePageTest.java
+++ SeleniumTwo/src/test/java/test/scripts/FindFooterSectionLinsOnMyntraHomePageTest.java
@@ -33,11 +33,14 @@ public class FindFooterSectionLinsOnMyntraHomePageTest extends MyntraHomePageObj
 		action.moveToElement(footerLinks).perform();
 		List<WebElement> links = footerLinks.findElements(By.tagName("a"));
 		System.out.println("Number of links in footer section: " + links.size());
-		  for (WebElement link : links) {
+		 /* for (WebElement link : links) {
 		        //String linkText = link.getText().trim();
 		        String href = link.getAttribute("href");
 		        System.out.println("Link URL: " + href);
-		    }
+		    }*/
+		WebElement popularSearchElement = driver.findElement(By.xpath(popularSearch));
+		List<WebElement> popularSearchLinks = popularSearchElement.findElements(By.tagName("a"));
+		System.out.println("Number of links in popular search section: "+popularSearchLinks.size());
 		
 	}
 	
