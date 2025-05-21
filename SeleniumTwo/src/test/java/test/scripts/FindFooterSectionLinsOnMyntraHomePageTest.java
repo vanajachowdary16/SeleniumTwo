f6963921d0754e52e44ab85e6a0f06af7ac09199
vanajachowdary16 <vnajachowdary12@gmail.com>
2025-05-21 23:08:59 +0530

    working with windowhandles

--- SeleniumTwo/src/test/java/test/scripts/FindFooterSectionLinsOnMyntraHomePageTest.java
+++ SeleniumTwo/src/test/java/test/scripts/FindFooterSectionLinsOnMyntraHomePageTest.java
@@ -1,8 +1,12 @@
 package test.scripts;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterSuite;
@@ -26,7 +30,7 @@ public class FindFooterSectionLinsOnMyntraHomePageTest extends MyntraHomePageObj
 	}
 	
 	@Test
-	public void findFooterSectionLinks() {
+	public void findFooterSectionLinks() throws InterruptedException {
 		
 		WebElement footerLinks =WebUtilis.findElementByxpath(driver, footerXpath);
 		Actions action = new Actions(driver);
@@ -41,6 +45,28 @@ public class FindFooterSectionLinsOnMyntraHomePageTest extends MyntraHomePageObj
 		WebElement popularSearchElement = driver.findElement(By.xpath(popularSearch));
 		List<WebElement> popularSearchLinks = popularSearchElement.findElements(By.tagName("a"));
 		System.out.println("Number of links in popular search section: "+popularSearchLinks.size());
+		for(int i=0;i<5;i++) {
+			System.out.println(popularSearchLinks.get(i).getAttribute("href"));
+			WebElement link = popularSearchLinks.get(i);
+			
+			String href = link.getAttribute("href");
+			((JavascriptExecutor) driver).executeScript("window.open(arguments[0])", href);
+			
+			//action.moveToElement(link).click();
+       // String clickonlinkTab=Keys.chord(Keys.CONTROL,Keys.ENTER);		
+       // popularSearchElement.findElements(By.tagName("a")).get(i).sendKeys(clickonlinkTab);
+			Thread.sleep(5000L);
+
+		}
+			Set<String> windowhandle=driver.getWindowHandles();
+			Iterator<String> it =windowhandle.iterator();
+			while(it.hasNext()) {
+				driver.switchTo().window(it.next());
+				String title = driver.getTitle();
+				System.out.println("window title is \"" + title + "\"");
+				Thread.sleep(3000);
+			}			
+		
 		
 	}
 	
