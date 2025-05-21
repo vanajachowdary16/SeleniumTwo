6c3a30ec5fcb9bcc2eb69e1575e50d17c035849a
vanajachowdary16 <vnajachowdary12@gmail.com>
2025-05-21 19:13:46 +0530

    learning action class via myntra

---
+++ SeleniumTwo/src/test/java/test/PageObjects/MyntraHomePageObjects.java
@@ -0,0 +1,13 @@
+package test.PageObjects;
+
+import Test.Uitilities.WebUtilis;
+
+public class MyntraHomePageObjects extends WebUtilis{
+	public static String myntraHomeURL="https://www.myntra.com/";
+	public static String menCssSelector="a.desktop-main[href='/shop/men']";
+	public static String TshirsXpath="//a[@class='desktop-categoryLink' and text()='T-Shirts']";
+	
+	
+
+
+}
