eb1833f85fb6eb28d617705893ae8b1861c747de
vanajachowdary16 <vnajachowdary12@gmail.com>
2025-05-22 12:31:33 +0530

    links

--- SeleniumTwo/src/test/java/test/scripts/FindFooterSectionLinsOnMyntraHomePageTest.java
+++ SeleniumTwo/src/test/java/test/scripts/FindFooterSectionLinsOnMyntraHomePageTest.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterSuite;
